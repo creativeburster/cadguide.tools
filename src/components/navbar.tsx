8c2a33b42717c255640530c33878e8255f7121f6
gstar-byte <gstar-byte@users.noreply.github.com>
2026-05-31 14:10:27 +0800

    Optimize navbar menu item to singular 'Guide' for UX

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar() {
     { name: 'Compare', href: '/compare' },
     { name: 'Matchmaker', href: '/matchmaker' },
     { name: 'Deals', href: '/deals' },
-    { name: 'Guides', href: '/guides' },
+    { name: 'Guide', href: '/guides' },
     { name: 'About', href: '/about' },
   ];
 
